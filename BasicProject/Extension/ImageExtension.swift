11afe7f7ef6ae64fc2ee51488fa31391ec7790c2
Ethan <rxswift@126.com>
2023-02-15 10:17:18 +0800

    Update ImageExtension.swift

--- BasicProject/Extension/ImageExtension.swift
+++ BasicProject/Extension/ImageExtension.swift
@@ -22,7 +22,7 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return image!
     }
-
+    
 }
 
 public enum ImageType {
@@ -35,12 +35,12 @@ public enum ImageType {
 }
 
 extension UIImageView: ETExtensionCompatible {
-
+    
 }
 extension ET where Base: UIImageView {
     
     /// 添加水印
-    func waterMark(_ image: UIImage,mark: String) -> UIImage?{
+    public func waterMark(_ image: UIImage,mark: String) -> UIImage?{
         let w = image.size.width
         let h = image.size.height
         UIGraphicsBeginImageContext(image.size)
@@ -51,5 +51,107 @@ extension ET where Base: UIImageView {
         let img = UIGraphicsGetImageFromCurrentImageContext();
         UIGraphicsEndImageContext();
         return img
+    }
+    
+    
+    /// 图片压缩
+    public func compressImage() -> Data {
+        let imageSize = self.jpegData(compressionQuality: 1.0)!.count/1024
+        var myImage = self
+        if imageSize < 300 {
+            // 如果小于300k,直接上传
+            return myImage.jpegData(compressionQuality: 1.0)!
+        } else {
+            // 取短边
+            let width = myImage.size.width > myImage.size.height ? myImage.size.height:myImage.size.width
+            if width <= 1080 {
+                // 大于300k短边小于1080，直接上传
+                return myImage.jpegData(compressionQuality: 1)!
+            } else {
+                // 待压缩的Size
+                var size: CGSize = CGSize.zero
+                // 如果宽大于高
+                if myImage.size.width > myImage.size.height {
+                    size = CGSize.init(width: myImage.size.width*(1080/myImage.size.height), height: 1080)
+                } else {
+                    size = CGSize.init(width: 1080, height: myImage.size.height*(1080/myImage.size.width))
+                }
+                // 尺寸压缩
+                UIGraphicsBeginImageContext(size)
+                myImage.draw(in: CGRect.init(x: 0, y: 0, width: size.width, height: size.height))
+                if let img = UIGraphicsGetImageFromCurrentImageContext() {
+                    myImage = img
+                }
+                UIGraphicsEndImageContext()
+                SDJGLog("width:\(myImage.size.width) height:\(myImage.size.height)")
+                if myImage.jpegData(compressionQuality: 1.0)!.count/1024 >= 300{
+                    // 尺寸压缩后还大于300k
+                    for index in 1...7{
+                        let rate = CGFloat(1) - 0.1*CGFloat(index)
+                        let count = myImage.jpegData(compressionQuality: rate)!.count/1024
+                        if count <= 300{
+                            return myImage.jpegData(compressionQuality: rate)!
+                        }
+                        // 系数0.3仍然大于300k上传
+                        if index == 7{
+                            return myImage.jpegData(compressionQuality: rate)!
+                        }
+                    }
+                }else{
+                    // 尺寸压缩后还小于300k
+                    return myImage.jpegData(compressionQuality: 1)!
+                }
+            }
+        }
+        return myImage.jpegData(compressionQuality: 1)!
+    }
+    
+    /// 图片压缩（文件大小）
+    /// - Parameter size: 最大文件大小
+    /// - Parameter unit: 大小单位
+    public func compressImageWithFileSize(size: Int, unit: String = "b") -> UIImage {
+        var maxSize: Int = 0
+        switch unit {
+        case "b", "B":
+            maxSize = size
+            break
+        case "k", "K":
+            maxSize = size * 1000
+            break
+        case "m", "M":
+            maxSize = size * 1000 * 1000
+            break
+        default:
+            maxSize = size
+            break
+        }
+        // 先压图片体积
+        var imageData = self.jpegData(compressionQuality: 1.0)!
+        var ratio: CGFloat = 1.0
+        while imageData.count >= maxSize && ratio >= 0.05 {  //压
+            ratio -= 0.05
+            let  newImage = UIImage.init(data: imageData)
+            imageData = newImage!.jpegData(compressionQuality: ratio)!
+        }
+        // 如果压图片之后未达到小于maxSize，则采用缩图片size
+        if ratio < 0.05 {
+            var sizeRatio: Float = 0.95
+            while imageData.count >= maxSize && sizeRatio >= 0.5 { //缩
+                imageData = UIImage(data: imageData)?.extCompressImage() ?? Data()
+                sizeRatio -= 0.05
+            }
+        }
+        return UIImage(data: imageData)!
+    }
+    
+    /// 合成两张图片
+    public func compoundImage(otherImage: UIImage)-> UIImage{
+        let size = self.size
+        let otherBounds = size.width > size.height ? size.height/2 : size.width/2
+        UIGraphicsBeginImageContext(size)
+        self.draw(in: CGRect.init(x:0, y:0, width:size.width, height:size.height))
+        otherImage.draw(in: CGRect.init(x:size.width/2-otherBounds/2, y:size.height/2-otherBounds/2, width:otherBounds, height:otherBounds))
+        let resultingImage = UIGraphicsGetImageFromCurrentImageContext()
+        return resultingImage!
     }
 }
